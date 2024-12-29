1b61207292a735deb7bd2c40808bfc97f308f003
vanoc <147881592+vshdev@users.noreply.github.com>
2024-12-29 13:05:33 -0700

    Update boot.asm

--- ki/boot.asm
+++ ki/boot.asm
@@ -1,23 +1,23 @@
 org 0x7c00
 bits 32
 
-cli                ; Disable interrupts
-xor ax, ax         ; Clear AX
-mov ss, ax         ; Set stack segment to 0x0000
-mov sp, 0x7C00     ; Set stack pointer just below bootloader
-sti                ; Enable interrupts
+cli
+xor ax, ax
+mov ss, ax
+mov sp, 0x7C00
+sti
 
-mov ax, 0x1000     ; ES: Segment to load kernel (0x1000:0000)
-mov es, ax         ; Set ES to 0x1000
-xor bx, bx         ; Start writing at offset 0x0000
-mov ah, 0x02       ; Function: Read sectors
-mov al, 16         ; Number of sectors to read (adjust for your kernel size)
-mov ch, 0x00       ; Cylinder 0
-mov cl, 0x02       ; Sector 2 (kernel starts here)
-mov dh, 0x00       ; Head 0
-mov dl, 0x00       ; Drive 0 (floppy disk)
-int 0x13           ; BIOS interrupt to read sectors
-jc disk_error      ; Jump if carry flag is set (read failed)
+mov ax, 0x1000     ; load to Here
+mov es, ax  
+xor bx, bx     
+mov ah, 0x02       ;Read f()
+mov al, 16
+mov ch, 0x00       ; cylinder 0
+mov cl, 0x02       ; sector 2
+mov dh, 0x00       ; head 0
+mov dl, 0x00       ; drive 0
+int 0x13
+jc disk_error
 
 jmp 0x1000:0000
 
